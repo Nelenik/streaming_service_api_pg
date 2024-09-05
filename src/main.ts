2cbb7b3bf9e4af17387d6a90406a96d3b776f7df
NeLeNik <lnek10.1990@gmail.com>
2024-09-06 00:45:49 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost'],
+    origin: ['http://localhost:8080'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
   });
