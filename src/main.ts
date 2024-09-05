7d128f25a6e57ee9475e37de17aede21ce2e44a9
NeLeNik <lnek10.1990@gmail.com>
2024-09-06 00:23:01 +0300

    cors settings

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ import * as express from 'express';
 import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
   });
 
   app.setGlobalPrefix('api');
