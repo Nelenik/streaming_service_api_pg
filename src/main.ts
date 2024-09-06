da25710d2e5bf0d431dc8ff8ffb9cc72a49aff38
NeLeNik <lnek10.1990@gmail.com>
2024-09-06 15:22:01 +0300

    changed cors origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: ['POST', 'GET', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   });
